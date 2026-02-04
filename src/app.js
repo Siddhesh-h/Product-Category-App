5e3a6b42ca1fe7f3469a5c0f456bf567390f68c8
Siddhesh Sarang <siddheshsarang73@gmail.com>
2026-02-04 12:50:18 +0530

    feat: finalize admin UI layout and UX improvements

--- src/app.js
+++ src/app.js
@@ -2,21 +2,22 @@ const express = require("express");
 const path = require("path");
 require("./config/db");
 
-const categoryRoutes = require("./routes/category.routes");
-const productRoutes = require("./routes/product.routes");
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+const categoryRoutes = require("./routes/category.routes");
+const productRoutes = require("./routes/product.routes");
 
 app.use("/categories", categoryRoutes);
 app.use("/products", productRoutes);
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.redirect("/categories");
 });
 
 module.exports = app;
